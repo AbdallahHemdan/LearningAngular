9ace6afb0f1ae8b044e516b90601d435ed4fbe60
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-07-06 01:50:29 +0200

    class binding and ngClass directive

--- hello-angular/src/app/ask/ask.component.ts
+++ hello-angular/src/app/ask/ask.component.ts
@@ -9,6 +9,12 @@ export class AskComponent implements OnInit {
   public name = 'Omar Hemdan';
   public userId = '123456789';
   public isInputDisabled = false;
+  public successClass = 'text-success';
+  public dangerClass = 'text-danger';
+  public specialClass = 'text-special';
+  public isSuccess = false;
+  public isError = !this.isSuccess;
+  public isSpecial = true;
 
   constructor() {}
 
