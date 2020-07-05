f931bea4370bb974aed1f42a9b66adeaf4fd1f4e
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-07-05 23:02:07 +0200

    learn more about components

---
+++ hello-angular/src/app/ask/ask.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ask',
+  templateUrl: './ask.component.html',
+  styleUrls: ['./ask.component.scss'],
+})
+export class AskComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
